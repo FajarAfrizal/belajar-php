143c0f6f41ddb3bda097d54c191a8e9f55fe5770
FajarAfrizal <fajarmuhammadafrizal@smkwikrama.sch.id>
2022-01-25 10:32:46 +0700

    first commit

---
+++ percabangan.php
@@ -0,0 +1,20 @@
+<?php
+//if
+$baju = true;
+
+if($baju){
+	echo "Variable baju bernilai true"; 
+}
+?>
+<?php 
+// if else
+$nilai = 50;
+
+if ($nilai > 70) {
+  echo "Selamat, anda lulus!";
+} else {
+  echo "Mohon maaf, anda harus mengulang";
+}
+
+echo "<br>";
+?>
---
+++ variable.php
@@ -0,0 +1,8 @@
+<?php
+   $nama = 'Novica Ardina'; 
+   $umur = 20; 
+    
+   echo "Nama saya adalah ", $nama; 
+   echo "<br>";
+   echo "Saya berumur $umur tahun";
+?>
